97da74b35d23bb6fdd49f717219a966937f194e2
Marcos Ostos <marcos.ostos@gmail.com>
2017-09-25 19:50:56 +0200

    add pug and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,11 +30,29 @@ module.exports = function(grunt) {
       }
     },
     
+    // Compile pug
+    pug: {
+      compile: {
+        options: {
+          data: {
+            debug: true
+          }
+        },
+        files: {
+          'build/index.html': ['src/pug/*.pug']
+        }
+      }
+    },
+    
+    
   });
   
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('eslint-grunt');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-pug');
   
-  
+  // Start web server
+  grunt.registerTask('build', ['eslint', 'uglify', 'sass', 'pug']);
 };
